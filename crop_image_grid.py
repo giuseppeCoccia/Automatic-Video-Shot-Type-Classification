cf8eadcf848b00a0f4895e783e0faa31d2238708
paolomoriello <paolomoriello94@gmail.com>
2018-04-16 22:49:03 +0200

    solved bug in crop

--- crop_image_grid.py
+++ crop_image_grid.py
@@ -4,7 +4,7 @@ import sys
 
 # read the image
 img_name = sys.argv[1]
-output_dir = sys.argv[2]
+output_dir = sys.argv[3]
 img = cv2.imread(img_name)
 
 
@@ -16,4 +16,4 @@ pixels = int(sys.argv[2])
 for i in range(pixels):
     for j in range(pixels):
         new_img = img[i:len_y, j:len_x]
-        cv2.imwrite(output_dir+'/cropped_'+str(i)+str(j)+'_'+img_name, crop_img)
+        cv2.imwrite(output_dir+'cropped_'+str(i)+str(j)+'_'+img_name, new_img)
