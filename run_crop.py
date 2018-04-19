5cb7f867a63448e9c525eb5376fbb869697123b3
paolomoriello <paolomoriello94@gmail.com>
2018-04-19 14:07:04 +0200

    changed run_script from bash to python

---
+++ run_crop.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+directory = sys.argv[1]
+pixels = 7
+
+for path, subdirs, files in os.walk(directory):
+    for name in files:
+        if ".jpg" in name:
+            img_path = os.path.join(path, name)
+            cropped_path = directory+"cropped_"+path.split("/")[-1]
+            if not os.path.exists(cropped_path):
+                print(cropped_path)
+                os.makedirs(cropped_path)
+            os.system('python crop_image_grid.py "'+img_path+'" '+str(pixels)+' "'+cropped_path+'/"')
+
