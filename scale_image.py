acfb426a8f8940f0f1d8cc68b0c7c1cb7f163fbe
Paolo Moriello <paolomoriello94@gmail.com>
2018-06-18 20:15:47 +0200

    added scaling script

--- scale_image.py
+++ scale_image.py
@@ -6,23 +6,19 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description="Script for flipping image")
 parser.add_argument('img_path', type=str, help='path to img')
-parser.add_argument('value', type=int, help='value of the brightness to be added')
+parser.add_argument('w', type=int, help='weight of the scaled image')
+parser.add_argument('h', type=int, help='heigh of the scaled image')
 parser.add_argument('-outdir', '--outdir', nargs='?', type=str, help='path to output dir. if none, it is saved in the same directory of the input image')
 
 args = parser.parse_args()
 img_path = args.img_path
-value = args.value
+w = args.w
+h = args.h
 output_dir = args.outdir
 
 # read the image
 img = cv2.imread(img_path)
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #convert it to hsv
-
-h, s, v = cv2.split(hsv)
-v = np.where((255-v)<value,255,v+value)
-final_hsv = cv2.merge((h, s, v))
-
-img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+img = cv2.resize(img, (w, h), interpolation=cv2.INTER_AREA)
 
 # save img
 img_name = os.path.basename(img_path)
